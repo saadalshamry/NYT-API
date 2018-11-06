f5ce89d8b3dd7e7b91f58246093f5cb15c7e7fde
Jetender <g.jetender@gmail>
2018-11-05 20:22:56 -0500

    good

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,16 @@
 //Submit button
-$("#submit").on("click", function(event) {
+$("#submit").on("click", function (event) {
 
-    var searchTerm = $("#searchTerm").text();
-    var numRecords = $("#numRecords").text();
-    var startYear = $("#startYear").text();
-    var endYear = $("#endYear").text();
+  var searchTerm = $("#searchTerm").text();
+  var numRecords = $("#numRecords").text();
+  var startYear = $("#startYear").text();
+  var endYear = $("#endYear").text();
 
-  });
-
-//Clear button
-$("#clear").on("click", function(event) {
-    
 });
 
+//Clear button
+$("#clear").on("click", function (event) {
 
+});
+
+////eflms;mfosrmg'kjwepjf p[ewkfgjoepwksfl'
