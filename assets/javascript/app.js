b749e10c0d86e6f86d0df4d565c96374b17f7281
saad <saad.alshamry.2012@gmail.com>
2018-11-05 20:46:10 -0500

    ajax pt1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,10 +3,24 @@
 //Submit button
 $("#submit").on("click", function (event) {
 
+  var key = "$api-key=e7f9620ade334806b2c913e061942370";
+  var apiurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key"
   var searchTerm = $("#searchTerm").text();
   var numRecords = $("#numRecords").text();
   var startYear = $("#startYear").text();
   var endYear = $("#endYear").text();
+
+  $.ajax({
+    url: url
+  }).then(function (response) {
+    var res = response.response.docs;
+    res.forEach(function (doc) {
+      var div = $('<div class="doc">');
+      var a = $('<a>')
+      a.attr('href', doc.web_url);
+      a.text('Visit URL')
+    });
+  });
 
 });
 
