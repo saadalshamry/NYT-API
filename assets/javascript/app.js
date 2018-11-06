c8afdbd1ddb779f88277b35cf5a99e7cae8d453d
papasnip <chrisfernandes123@hotmail.com>
2018-11-05 20:15:23 -0500

    201811052015

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+//Submit button
+$("#submit").on("click", function(event) {
+
+    var searchTerm = $("#searchTerm").text();
+    var numRecords = $("#numRecords").text();
+    var startYear = $("#startYear").text();
+    var endYear = $("#endYear").text();
+
+  });
+
+//Clear button
+$("#clear").on("click", function(event) {
+    
+});
+
+
